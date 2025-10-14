d72189b909b04a5de2d3baa5d5e15722beffa21a
Rizky Maulana <rizky1517@gmail.com>
2025-10-14 14:02:53 +0700

    refactor(main): rename main class

--- src/main/java/com/unindra/Main.java
+++ src/main/java/com/unindra/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackEnInternshipTaskApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackEnInternshipTaskApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
