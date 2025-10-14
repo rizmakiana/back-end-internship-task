d55e385e97337e7580f288bf333226243ba4b1f1
Rizky Maulana <rizky1517@gmail.com>
2025-10-14 08:30:49 +0700

    add structure folder

---
+++ src/main/java/com/unindra/BackEnInternshipTaskApplication.java
@@ -0,0 +1,13 @@
+package com.unindra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEnInternshipTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEnInternshipTaskApplication.class, args);
+	}
+
+}
